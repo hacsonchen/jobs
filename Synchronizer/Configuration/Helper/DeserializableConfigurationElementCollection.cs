4de9cd2690cfe8e78a9e30ec5340f7e322389fc6
chenqingsen <chenqingsen@96900.com.cn>
2017-10-09 14:53:51 +0800

    changed the configration of request

---
+++ Synchronizer/Configuration/Helper/DeserializableConfigurationElementCollection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synchronizer.Configuration.Helper
+{
+	public abstract class DeserializableConfigurationElementCollection<TElement> :
+		DeserializableConfigurationElementCollectionBase<TElement>
+		where TElement : DeserializableConfigurationElement, new()
+	{
+		/// <summary>
+		/// When overridden in a derived class, creates a new <see cref="T:System.Configuration.ConfigurationElement"/>.
+		/// </summary>
+		/// <returns>
+		/// A new <see cref="T:System.Configuration.ConfigurationElement"/>.
+		/// </returns>
+		protected override ConfigurationElement CreateNewElement()
+		{
+			return new TElement();
+		}
+	}
+}
---
+++ Synchronizer/Configuration/RequestElementCollection.cs
@@ -0,0 +1,24 @@
+﻿using Synchronizer.Configuration.Helper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synchronizer.Configuration
+{
+	[ConfigurationCollection(typeof(RequestElement))]
+	public class RequestElementCollection : DeserializableConfigurationElementCollection<RequestElement>
+	{
+		protected override ConfigurationElement CreateNewElement()
+		{
+			return new RequestElement();
+		}
+
+		protected override object GetElementKey(ConfigurationElement element)
+		{
+			return ((RequestElement)element).Name;
+		}
+	}
+}
